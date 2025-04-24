5b2c0bbe0c993fdc9b273f4fe1cfcf1111cb3d58
Francis <francisnjaramba2@gmail.com>
2025-04-24 10:13:06 +0300

    All changes

---
+++ fb.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbmvLzHNXKJw6SjhfUam_iwPeCAeTqTQs",
+    authDomain: "contact-form-2a96c.firebaseapp.com",
+    databaseURL: "https://contact-form-2a96c-default-rtdb.firebaseio.com",
+    projectId: "contact-form-2a96c",
+    storageBucket: "contact-form-2a96c.firebasestorage.app",
+    messagingSenderId: "751151548631",
+    appId: "1:751151548631:web:2b950ccb98fcf1a2f18892"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
